2df00852c8748bfde9aeced8c9a0e656f6ea7d40
Rhesa Davinanto <rhesa.davinanto@nadihealth.com>
2022-11-25 05:30:07 +0700

    first commit

---
+++ src/components/ModalDelete.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { httpServices } from "../services/httpServices";
+import logoWarning from '../assets/warningDeleteIcon.png'
+import Alert from "./Alert";
+
+export default function ModalDelete({
+  openModal,
+  closeModal,
+  isActivity,
+  data,
+  handleDelete
+}) {
+  return (
+    <>
+      {openModal ? (
+        <div
+          onClick={closeModal}
+          className="flex justify-center bg-gray-500 bg-opacity-75 items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="relative w-auto my-6 mx-auto max-w-3xl"
+          >
+            <div className="border-0 p-4 gap-5 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="flex justify-center p-10">
+              <img src={logoWarning} className='w-10 h-10' alt="" />
+              </div>
+              <h1>
+                Apakah anda yakin menghapus{" "}
+                {isActivity ? "activity" : "List Item"}{" "}
+                <span className="font-bold">“{data.title}”</span>?
+              </h1>
+              <div className="flex gap-5 justify-center">
+                <button
+                  onClick={closeModal}
+                  className="bg-gray-500 rounded-full px-5 py-2"
+                >
+                  Batal
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="bg-red-500 rounded-full px-5 py-2"
+                >
+                  Hapus
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
